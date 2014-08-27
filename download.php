cdc394f26025d2aedbe7cceea3ec52f8b23c4f89
freeneo <freeneo@linux-njay.local>
2014-08-27 14:36:24 +0900

    simplify code

--- download.php
+++ download.php
@@ -5,6 +5,8 @@ if( ! isset($_GET['id']) or empty($_GET['id'])){
 }
 
 include '../../../wp-blog-header.php';
+include 'Browser.php/lib/Browser.php';
+
 $filename_for_download = nlf_get_filename_for_download($_GET['id']);
 
 $attachment = get_post($_GET['id']);
@@ -13,17 +15,20 @@ $upload_dir = wp_upload_dir();
 
 $original_filepath = $upload_dir['basedir'] . '/' . $attachment_meta['_wp_attached_file'][0];
 
-if( strstr($_SERVER['HTTP_USER_AGENT'],'Firefox') OR strstr($_SERVER['HTTP_USER_AGENT'],'iPad') 
-        OR strstr($_SERVER['HTTP_USER_AGENT'],'iPod') 
-        OR strstr($_SERVER['HTTP_USER_AGENT'],'iPhone')){
-    $filename_encoded = $filename_for_download;
-}else{
+$browser = new Browser();
+
+if( 
+	$browser->getBrowser() == Browser::BROWSER_IE 
+){
     $filename_encoded = rawurlencode($filename_for_download);
+}else{
+	$filename_encoded = $filename_for_download;
 }
 
 status_header(200);
+header('cache-control: no-cache');
 header('Content-type: ' . $attachment->post_mime_type);
-header('Content-Disposition: attachment; filename="' . $filename_for_download . '"');
+header("Content-Disposition: attachment; filename=" . $filename_encoded);
 
 // orifinal file
 readfile($original_filepath);
