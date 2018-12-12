4c867f94c4dc8ff31d4a5e80c121fed7c78c7036
An, Hyeong-woo <mytory@gmail.com>
2018-12-12 12:22:04 +0900

    Fix a bug that if you try to download ID not exist, then it download empty file.
    취약점이라는데 재연이 안 된다. 아마 빈 파일 다운로드되는 것만으로 보고한 듯하다.
    https://cxsecurity.com/ascii/WLB-2018110241

--- download.php
+++ download.php
@@ -1,18 +1,20 @@
 <?php
-if( ! isset($_GET['id']) or empty($_GET['id'])){
+if (! isset($_GET['id']) or empty($_GET['id']) or ! is_numeric($_GET['id'])) {
     echo "error! enter attachment ID!";
     exit;
 }
 
 include '../../../wp-blog-header.php';
 
-$filename_for_download = nlf_get_filename_for_download($_GET['id']);
+$id = (int) $_GET['id'];
 
-$attachment = get_post($_GET['id']);
-$attachment_meta = get_post_meta($_GET['id']);
-$upload_dir = wp_upload_dir();
+$filename_for_download = nlf_get_filename_for_download($id);
 
-$original_path = $upload_dir['basedir'] . '/' . $attachment_meta['_wp_attached_file'][0];
+$attachment      = get_post($id);
+$attachment_meta = get_post_meta($id);
+$upload_dir      = wp_upload_dir();
+
+$original_path    = $upload_dir['basedir'] . '/' . $attachment_meta['_wp_attached_file'][0];
 $filename_encoded = rawurlencode($filename_for_download);
 
 status_header(200);
